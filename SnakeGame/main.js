0859ca7505b54f750fe8e4f049d657bc38e30a9f
Shai Dickman <803951@seq.org>
2021-08-20 16:26:51 -0700

    new classes

--- SnakeGame/main.js
+++ SnakeGame/main.js
@@ -1,52 +1,4 @@
 var snake,canvas,context,direction;
-class Snake{
-  constructor(segments,length){
-    this.segments = segments;
-    this.length = length;
-  }
-  move = function(){
-    if(this.segments.length>0){
-      let length = this.segments[0].length;
-      this.segments[0].x+=this.length*direction.x;
-      this.segments[0].y+=this.length*direction.y;
-
-      if(this.segments[0].x<length){
-        this.segments[0].x = canvas.width-this.length;
-      }
-      else if(this.segments[0].x>canvas.width-length){
-        this.segments[0].x = this.length;
-      }
-      if(this.segments[0].yy<this.length){
-        this.segments[0].y = canvas.height-this.length;
-      }
-      else if(this.segments[0].y>canvas.height-this.length){
-        this.segments[0].y = this.length;
-      }
-    }
-    for(var i = 1;i<this.segments.length;i++){
-      this.segments[i].x = this.segments[i-1].x;
-      this.segments[i].y = this.segments[i-1].y;
-    }
-  }
-}
-class SnakeSegment{
-  constructor(x,y,length,color){
-      this.x = x;
-      this.y = y;
-      this.length = length;
-      this.color = color;
-  }
-  draw = function(){
-    context.fillRect(this.x,this.y,this.length,this.length);
-    context.fillStyle = this.color;
-  }
-}
-class Vector{
-  constructor(x,y){
-    this.x = x;
-    this.y = y;
-  }
-}
 
 window.addEventListener("load", init);
 document.addEventListener('keydown', function(event) {
@@ -102,5 +54,5 @@ function animate() {
 }
 function update(){
   context.clearRect(0,0,canvas.width,canvas.height);
-  snake.move();
+  snake.updateSnake();
 }
