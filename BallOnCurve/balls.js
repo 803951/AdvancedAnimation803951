2f53e09a14409b053c9b48606ea52937fd892a06
Shai Dickman <803951@seq.org>
2021-08-23 12:24:12 -0700

    ball added

---
+++ BallOnCurve/balls.js
@@ -0,0 +1,16 @@
+class Ball{
+  constructor(x,y,radius,color){
+    this.canvas = document.getElementById("cnv");
+    this.context = this.canvas.getContext("2d");
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw = function(){
+    this.context.beginPath();
+    this.context.arc(this.x+this.canvas.width/2, this.canvas.height-this.y, this.radius, 0, 2 * Math.PI);
+    this.context.fillStyle = this.color;
+    this.context.fill();
+  }
+}
