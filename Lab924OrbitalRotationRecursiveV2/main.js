6f6079210e10bb4311ac7cb9ba505b8e4615780b
Shai Dickman <shai.j.dickman@gmail.com>
2021-09-26 12:40:58 -0700

    canvas size = window size

--- Lab924OrbitalRotationRecursiveV2/main.js
+++ Lab924OrbitalRotationRecursiveV2/main.js
@@ -5,9 +5,11 @@ var cnv,ctx,planets,background;
 function init(){
 
   planets = [];
+  background = new Color(0,0,0,0.06);
 
   cnv = document.getElementById("cnv");
   ctx = cnv.getContext("2d");
+  resetCanvas();
 
   let totalPlanetsR = 1;
   let totalPlanetsG = 1;
@@ -17,25 +19,29 @@ function init(){
   generateCustomPlanets(3,totalPlanetsG,1,255,1);
   generateCustomPlanets(3,totalPlanetsB,1,1,255);
 
-  background = new Color(0,0,0,0.06);
-  ctx.fillStyle = "black";
-  ctx.fillRect(0,0,cnv.width,cnv.height);
-
   animate();
 }
 
 function generateCustomPlanets(orbits,n,scaleR,scaleG,scaleB){
   let minR = 30;
   let maxR = 50;
-  let minOrbits = 4;
-  let maxOrbits = 6;
+  let minOrbits = 3;
+  let maxOrbits = 5;
   for(var i = 0;i<n;i++){
     var planet = Planet.generateRandomPlanet(minR,maxR,minOrbits,maxOrbits,orbits,scaleR,scaleG,scaleB);
     planets.push(planet);
   }
 }
-
+function resetCanvas(){
+  cnv.width = window.innerWidth;
+  cnv.height = window.innerHeight;
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,cnv.width,cnv.height);
+}
 function animate(){
+  if(window.innerWidth!=cnv.width||window.innerHeight!=cnv.height){
+    resetCanvas();
+  }
   ctx.fillStyle = background.toString();
   ctx.fillRect(0,0,cnv.width,cnv.height);
   update();
