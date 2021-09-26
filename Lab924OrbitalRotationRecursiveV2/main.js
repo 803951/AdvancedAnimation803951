18d48bc5c8ebb6c5fec5d398091268d037e88e26
Shai Dickman <shai.j.dickman@gmail.com>
2021-09-25 18:11:19 -0700

    cleaning up code

--- Lab924OrbitalRotationRecursiveV2/main.js
+++ Lab924OrbitalRotationRecursiveV2/main.js
@@ -12,27 +12,27 @@ function init(){
   let totalPlanetsR = 1;
   let totalPlanetsG = 1;
   let totalPlanetsB = 1;
-  let minR = 40;
-  let maxR = 60;
-  let orbits = 3;
-  for(var i = 0;i<totalPlanetsR;i++){
-    var planet = Planet.generateRandomPlanet(minR,maxR,4,6,orbits,255,1,1);
-    planets.push(planet);
-  }
-  for(var i = 0;i<totalPlanetsG;i++){
-    var planet = Planet.generateRandomPlanet(minR,maxR,4,6,orbits,1,255,1);
-    planets.push(planet);
-  }
-  for(var i = 0;i<totalPlanetsB;i++){
-    var planet = Planet.generateRandomPlanet(minR,maxR,4,6,orbits,1,1,255);
-    planets.push(planet);
-  }
+
+  generateCustomPlanets(3,totalPlanetsR,255,1,1);
+  generateCustomPlanets(3,totalPlanetsG,1,255,1);
+  generateCustomPlanets(3,totalPlanetsB,1,1,255);
 
   background = new Color(0,0,0,0.06);
   ctx.fillStyle = "black";
   ctx.fillRect(0,0,cnv.width,cnv.height);
 
   animate();
+}
+
+function generateCustomPlanets(orbits,n,scaleR,scaleG,scaleB){
+  let minR = 30;
+  let maxR = 50;
+  let minOrbits = 4;
+  let maxOrbits = 6;
+  for(var i = 0;i<n;i++){
+    var planet = Planet.generateRandomPlanet(minR,maxR,minOrbits,maxOrbits,orbits,scaleR,scaleG,scaleB);
+    planets.push(planet);
+  }
 }
 
 function animate(){
