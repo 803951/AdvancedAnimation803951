a59f04b68325f6360d822577ab1ac33a8b90f8e0
Shai Dickman <803951@seq.org>
2021-11-15 13:49:03 -0800

    axes correct position set up

--- 1103EcosystemPhase3/world.js
+++ 1103EcosystemPhase3/world.js
@@ -4,8 +4,59 @@ function World(w,h){
   this.ctx1 = cnv1.getContext("2d");
   this.cnv2 = document.getElementById("cnv2");
   this.ctx2 = cnv2.getContext("2d");
+
+  this.ctx1Pos = new JSVector(this.cnv1.width/2,this.cnv1.height/2);
+  this.ctx1TargetPos = new JSVector(this.ctx1Pos.x,this.ctx1Pos.y);
 }
 
 World.prototype.update = function(){
-  
+
+  this.lerpPosition(0.18)
+  this.draw();
+
+}
+
+World.prototype.lerpPosition = function(scale){
+
+  let diff = JSVector.subGetNew(this.ctx1TargetPos,this.ctx1Pos);
+  let mag = diff.getMagnitude();
+  diff.setMagnitude(mag*scale);
+  this.ctx1Pos.add(diff);
+
+}
+
+World.prototype.draw = function(){
+
+  this.ctx1.clearRect(0,0,this.dimensions.x,this.dimensions.y);
+  this.ctx2.clearRect(0,0,this.dimensions.x,this.dimensions.y);
+
+  this.ctx1.lineWidth = 5;
+  this.ctx1.strokeStyle = "red";
+  this.ctx1.save();
+  this.ctx1.translate(this.ctx1Pos.x,this.ctx1Pos.y);
+
+  this.ctx1.beginPath();
+  this.ctx1.moveTo(0,this.dimensions.y/2);
+  this.ctx1.lineTo(0,-this.dimensions.y/2);
+  this.ctx1.moveTo(this.dimensions.x/2,0);
+  this.ctx1.lineTo(-this.dimensions.x/2,0);
+  this.ctx1.stroke();
+  this.ctx1.restore();
+
+  let xScale = this.cnv2.width/this.dimensions.x;
+  let yScale = this.cnv2.height/this.dimensions.y;
+
+  this.ctx2.save();
+  this.ctx2.scale(xScale,yScale);
+  this.ctx2.translate(this.dimensions.x/2,this.dimensions.y/2);
+  this.ctx2.lineWidth = 5;
+  this.ctx2.strokeStyle = "red";
+  this.ctx2.beginPath();
+  this.ctx2.moveTo(0,this.dimensions.y/2);
+  this.ctx2.lineTo(0,-this.dimensions.y/2);
+  this.ctx2.moveTo(this.dimensions.x/2,0);
+  this.ctx2.lineTo(-this.dimensions.x/2,0);
+  this.ctx2.stroke();
+  this.ctx2.restore();
+
 }
