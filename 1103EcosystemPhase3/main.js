685aca5201dd9418685da230d92f6c6159b0d31d
Shai Dickman <803951@seq.org>
2021-11-15 16:56:23 -0800

    check out much better code Mr Kary!  🙂

--- 1103EcosystemPhase3/main.js
+++ 1103EcosystemPhase3/main.js
@@ -1,8 +1,41 @@
 window.addEventListener("load", init);
 
-var world;
+var world,controls;
 
+window.addEventListener("keydown", function(event){
+  if(event.code==="KeyW"){
+    controls.up = true;
+    controls.down = false;
+  }
+  else if(event.code==="KeyS"){
+    controls.down = true;
+    controls.up = false;
+  }
+  if(event.code==="KeyA"){
+    controls.left = true;
+    controls.right = false;
+  }
+  else if(event.code==="KeyD"){
+    controls.right = true;
+    controls.left = false;
+  }
+});
+window.addEventListener("keyup", function(event){
+  if(event.code==="KeyW"){
+    controls.up = false;
+  }
+  else if(event.code==="KeyS"){
+    controls.down = false;
+  }
+  if(event.code==="KeyA"){
+    controls.left = false;
+  }
+  else if(event.code==="KeyD"){
+    controls.right = false;
+  }
+});
 function init(){
+  controls = {left:false,right:false,up:false,down:false};
   world = new World(2000,2000);
   animate();
 }
--- 1103EcosystemPhase3/world.js
+++ 1103EcosystemPhase3/world.js
@@ -1,19 +1,55 @@
+var targetPos;
+
 function World(w,h){
+
   this.dimensions = new JSVector(w,h);
   this.cnv1 = document.getElementById("cnv1");
   this.ctx1 = cnv1.getContext("2d");
   this.cnv2 = document.getElementById("cnv2");
   this.ctx2 = cnv2.getContext("2d");
 
-  this.ctx1Pos = new JSVector(this.cnv1.width/2,this.cnv1.height/2);
+  this.ctx1Pos = new JSVector(-this.cnv1.width/2,-this.cnv1.height/2);
   this.ctx1TargetPos = new JSVector(this.ctx1Pos.x,this.ctx1Pos.y);
+  targetPos = new JSVector(0,0);
+  this.movementSpeed = 1;
+
+  this.cnv2.addEventListener("click",function(event){
+    let x = event.offsetX;
+    let y = event.offsetY;
+    let targetX = x-this.width/2;
+    let targetY = y-this.height/2;
+    targetPos.x = targetX;
+    targetPos.y = targetY;
+  });
 }
 
 World.prototype.update = function(){
 
+  this.updatePosition();
   this.lerpPosition(0.18)
   this.draw();
 
+}
+
+World.prototype.updatePosition = function(){
+
+  let delta = new JSVector(0,0);
+  if(controls.up){
+    delta.y -= this.movementSpeed;
+  }
+  else if(controls.down){
+    delta.y += this.movementSpeed;
+  }
+  if(controls.left){
+    delta.x -= this.movementSpeed;
+  }
+  else if(controls.right){
+    delta.x += this.movementSpeed;
+  }
+  if(delta.getMagnitude()>0) delta.setMagnitude(this.movementSpeed);
+  targetPos.add(delta);
+  this.ctx1TargetPos.x = targetPos.x*this.dimensions.x/this.cnv2.width-this.cnv1.width/2;
+  this.ctx1TargetPos.y = targetPos.y*this.dimensions.y/this.cnv2.height-this.cnv1.height/2;
 }
 
 World.prototype.lerpPosition = function(scale){
@@ -33,7 +69,7 @@ World.prototype.draw = function(){
   this.ctx1.lineWidth = 5;
   this.ctx1.strokeStyle = "red";
   this.ctx1.save();
-  this.ctx1.translate(this.ctx1Pos.x,this.ctx1Pos.y);
+  this.ctx1.translate(-this.ctx1Pos.x,-this.ctx1Pos.y);
 
   this.ctx1.beginPath();
   this.ctx1.moveTo(0,this.dimensions.y/2);
@@ -57,6 +93,7 @@ World.prototype.draw = function(){
   this.ctx2.moveTo(this.dimensions.x/2,0);
   this.ctx2.lineTo(-this.dimensions.x/2,0);
   this.ctx2.stroke();
+  this.ctx2.strokeRect(this.ctx1Pos.x, this.ctx1Pos.y, this.cnv1.width, this.cnv1.height);
   this.ctx2.restore();
 
 }
